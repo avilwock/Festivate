5da5522c900fc492d641c0cd81621eb4a52fc0cc
Marisol514 <145890422+Marisol514@users.noreply.github.com>
2024-05-28 21:46:40 -0700

    updated the typeDef's

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -12,10 +12,37 @@ type Query {
     user: User!
   }
   
-  type Mutation {
-    login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, password: String!): Auth
+  type Event {
+    _id: ID!
+    name: String!
+    date: String!
+    location: String
+    tasksList: [Task]
   }
+
+  type Task {
+    _id: ID!
+    description: String!
+    event: String!
+  }
+
+  input TaskInput {
+    _id: ID!
+    description: String!
+  }
+
+type Mutation {
+  login(email: String!, password: String!): Auth
+  addUser(username: String!, email: String!, password: String!): Auth
+  addEvent(name: String!, date: String!, location: String!): Event
+  addTask(description: String!): Event
+  completeTask(taskId: ID): Event
+}
+
+schema {
+  query: Query
+  mutation: Mutation
+}
 `;
 
 module.exports = typeDefs;
