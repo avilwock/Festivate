1ab872afc5e617802ecc90f79014fc7088b13774
avilwock <151412286+avilwock@users.noreply.github.com>
2024-05-28 20:35:06 -0700

    fixed bug

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,15 +1,21 @@
 const typeDefs = `
+type Query {
+  me: User
+}
   type User {
     _id: ID!
     username: String!
     email: String!
+  }
+  type Auth {
+    token: String!
+    user: User!
   }
   
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
   }
- 
 `;
 
 module.exports = typeDefs;
