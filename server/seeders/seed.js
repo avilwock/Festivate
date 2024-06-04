e7e4d65138cc6441cf1db72d3ea69e1fc327bd34
Marisol514 <145890422+Marisol514@users.noreply.github.com>
2024-06-03 21:38:01 -0700

    udated the profile page and addes seed files

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,17 +1,42 @@
 const db = require('../config/connection');
-const { User } = require('../models/User');
+const { User, Event, Task } = require('../models');
 const userSeeds = require('./userSeeds.json');
-//const cleanDB = require('./cleanDB');
+const eventSeeds = require('./eventSeeds.json');
+const taskSeeds = require('./taskSeeds.json');
 
 db.once('open', async () => {
     try {
-        //await cleanDB('Profile', 'profiles');
+        // Clean up the database
+        await User.deleteMany({});
+        await Event.deleteMany({});
+        await Task.deleteMany({});
 
-        await User.create(userSeeds);
+        // Seed users
+        const users = await User.create(userSeeds);
 
-        console.log('all done!');
+        // Map user IDs to events
+        const events = eventSeeds.map((event, index) => ({
+            ...event,
+            user: users[index % users.length]._id
+        }));
+
+        // Seed events
+        const createdEvents = await Event.insertMany(events);
+
+        // Map event and user IDs to tasks
+        const tasks = taskSeeds.map((task, index) => ({
+            ...task,
+            user: users[index % users.length]._id,
+            event: createdEvents[index % createdEvents.length]._id
+        }));
+
+        // Seed tasks
+        await Task.insertMany(tasks);
+
+        console.log('Database seeded successfully!');
         process.exit(0);
     } catch (err) {
-        throw err;
+        console.error(err);
+        process.exit(1);
     }
 });
