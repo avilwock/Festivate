d6fa712e21e2cbcc90f86174168e28e3fec6e3ac
avilwock <151412286+avilwock@users.noreply.github.com>
2024-06-10 15:32:36 -0700

    second attempt at installation

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -28,18 +28,6 @@ const router = createBrowserRouter([
   },
 ]);
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then((registration) => {
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      })
-      .catch((error) => {
-        console.log('ServiceWorker registration failed: ', error);
-      });
-  });
-}
-
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
