7f749b800c801f3538bc0bf59a675bd3939ef93f
avilwock <151412286+avilwock@users.noreply.github.com>
2024-06-10 15:23:11 -0700

    got installation button working

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,45 +1,14 @@
-// client/src/components/Header/index.jsx
+// Header.jsx
+// import React from 'react';
 import { Link } from 'react-router-dom';
 import Auth from '../../utils/auth';
+import InstallButton from '../InstallButton';
 
 const Header = () => {
   const logout = (event) => {
     event.preventDefault();
     Auth.logout();
   };
-
-  /*
-  return (
-    <header className="bg-info mb-4 py-3 display-flex align-center">
-      <div className="container flex-column justify-space-between-lg justify-center align-center text-center">
-        <Link className="header-link" to="/">
-          <h3 className="m-0" style={{ fontSize: '3rem' }}>Festivate</h3>
-        </Link>
-        <Link className="header-link" to="/">
-          <p className="m-0" style={{ fontSize: '1.75rem', fontWeight: '700' }}>Home</p>
-        </Link>
-        <div>
-          {Auth.loggedIn() ? (
-            <>
-              <Link className="btn btn-lg btn-primary m-2" to="/me">View My Profile</Link>
-              <Link className="btn btn-lg btn-primary m-2" to="/events">Events</Link>
-              <button className="btn btn-lg btn-light m-2" onClick={logout}>Logout</button>
-            </>
-          ) : (
-            <>
-              <Link className="btn btn-lg btn-primary m-2" to="/login">Login</Link>
-              <Link className="btn btn-lg btn-light m-2" to="/signup">Signup</Link>
-            </>
-          )}
-        </div>
-      </div>
-    </header>
-  );
-  };
-  
-  export default Header;
-  
-  */
 
   return (
     <header className="header bg-info mb-4 py-3">
@@ -61,6 +30,7 @@ const Header = () => {
               <Link className="btn btn-lg btn-light m-2" to="/signup">Signup</Link>
             </>
           )}
+          <InstallButton />
         </nav>
       </div>
     </header>
